8b3c2467ac9992143efcd648348e7153d2b6097e
Maxim Zhiburt <zhiburt@gmail.com>
2021-08-30 18:45:56 +0300

    Update ping example

--- examples/ping.rs
+++ examples/ping.rs
@@ -3,15 +3,14 @@ use std::io::prelude::*;
 fn main() {
     let proc = conpty::spawn("ping").unwrap();
     let mut reader = proc.output().unwrap();
-    let mut writer = proc.input().unwrap();
 
     println!("Process has pid={}", proc.pid());
 
-    writer.write(b"ping").unwrap();
+    while proc.is_alive() {};
 
-    let mut buf = [0; 300];
-    while proc.is_alive() {
-        let n = reader.read(&mut buf).unwrap();
-        println!("{}", String::from_utf8_lossy(&buf[..n]));
-    }
+    // currently I didn't figure out  way how to savely && easily read from pipe until EOF
+    let mut buf = [0; 1028 * 10 * 10];
+    let n = reader.read(&mut buf).unwrap();
+    println!("{:?}", &buf[..n]);
+    println!("{:?}", String::from_utf8_lossy(&buf[..n]));
 }
