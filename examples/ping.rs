09de0dfaea0c4f11816d61ca8f8514d43a040958
Maxim Zhiburt <zhiburt@gmail.com>
2021-09-06 15:20:40 +0300

    Refactor ping example

--- examples/ping.rs
+++ examples/ping.rs
@@ -6,11 +6,9 @@ fn main() {
 
     println!("Process has pid={}", proc.pid());
 
-    while proc.is_alive() {}
+    proc.wait(None).unwrap();
 
-    // currently I didn't figure out  way how to savely && easily read from pipe until EOF
     let mut buf = [0; 1028 * 10 * 10];
     let n = reader.read(&mut buf).unwrap();
-    println!("{:?}", &buf[..n]);
-    println!("{:?}", String::from_utf8_lossy(&buf[..n]));
+    println!("{}", String::from_utf8_lossy(&buf[..n]));
 }
