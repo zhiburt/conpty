0ac45a49782cb8ae1c4420d84281ed14323ff3e3
Maxim Zhiburt <zhiburt@gmail.com>
2022-12-24 20:01:54 +0300

    Update tests
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ tests/test_process.rs
@@ -0,0 +1,87 @@
+use std::{
+    io::{BufRead, BufReader},
+    path::Path,
+    process::Command,
+};
+
+use conpty::Process;
+
+#[test]
+pub fn envs() {
+    // note: maybe making cmd /C being a default as it was is good?
+    let batch = r#"cmd /C if "%TEST_ENV%"=="123456" (exit 0) else (exit 1)"#;
+
+    // set correct value
+    let mut cmd = Command::new(batch);
+    cmd.env("TEST_ENV".to_string(), "123456".to_string());
+    let proc = Process::spawn(cmd).unwrap();
+    assert_eq!(proc.wait(None).unwrap(), 0);
+
+    // set wrong value
+    let mut cmd = Command::new(batch);
+    cmd.env("TEST_ENV".to_string(), "NOT_CORRENT_VALUE".to_string());
+    let proc = Process::spawn(cmd).unwrap();
+    assert_eq!(proc.wait(None).unwrap(), 1);
+
+    // not set at all
+    let cmd = Command::new(batch);
+    let proc = Process::spawn(cmd).unwrap();
+    assert_eq!(proc.wait(None).unwrap(), 1);
+}
+
+#[test]
+pub fn test_args_0() {
+    // set correct value
+    let mut cmd = Command::new("cmd /C echo");
+    cmd.arg("Hello");
+    cmd.arg("World");
+
+    let mut proc = Process::spawn(cmd).unwrap();
+    let output = proc.output().unwrap();
+
+    let mut reader = BufReader::new(output);
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    assert_eq!(line, "\u{1b}[2J\u{1b}[m\u{1b}[HHello World\r\n");
+}
+
+#[test]
+pub fn test_args_1() {
+    // set correct value
+    let mut cmd = Command::new("cmd /C echo");
+    cmd.args(["Hello", "World", "!!!"]);
+
+    let mut proc = Process::spawn(cmd).unwrap();
+    let output = proc.output().unwrap();
+
+    let mut reader = BufReader::new(output);
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    assert_eq!(line, "\u{1b}[2J\u{1b}[m\u{1b}[HHello World !!!\r\n");
+}
+
+#[test]
+pub fn test_current_dir() {
+    // set correct value
+    let mut cmd = Command::new("cmd /C dir");
+    cmd.current_dir("./tests");
+
+    let mut proc = Process::spawn(cmd).unwrap();
+    let output = proc.output().unwrap();
+    let mut reader = BufReader::new(output);
+
+    let this_file_name = Path::new(file!()).file_name().unwrap().to_str().unwrap();
+    loop {
+        let mut line = String::new();
+        let i = reader.read_line(&mut line).unwrap();
+        if line.contains(this_file_name) {
+            return;
+        }
+
+        if i == 0 {
+            assert!(false)
+        }
+    }
+}
